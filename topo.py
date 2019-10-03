dbb22cf7822b9c8a42ed1f775d58ebe99e5925ac
sebitas0623 <sebastian.gomez3@udea.edu.co>
2019-10-03 11:13:07 +0000

    final

--- topo.py
+++ topo.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python
 
-#  Copyright 2019-present Open Networking Foundation
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+#-------------------------------------------------------
+#----------------- By: SEBASTIAN GOMEZ -----------------
+#-------------- Universidad de Antioquia ---------------
+#-------------------------------------------------------
 
 import argparse
 
@@ -25,8 +16,7 @@ from mininet.topo import Topo
 from bmv2 import ONOSBmv2Switch, ONOSHost
 
 CPU_PORT = 255
-pipeconf_name = "org.p4.srv6-tutorial"
-#pipeconf_name = "p4-tutorial-pipeconf"
+pipeconf_name = "org.p4.template"
 
 class TutorialTopo(Topo):
 
@@ -34,18 +24,18 @@ class TutorialTopo(Topo):
         Topo.__init__(self, *args, **kwargs)
 
         s1 = self.addSwitch('s1', cls=ONOSBmv2Switch, grpcport=50001, thriftport=50004,
-                               cpuport=CPU_PORT, pipeconf=pipeconf_name) #pipeconf=pipeconf_name, elogger=True)
+                               cpuport=CPU_PORT, pipeconf=pipeconf_name, loglevel="debug") 
         s2 = self.addSwitch('s2', cls=ONOSBmv2Switch, grpcport=50002, thriftport=50005,
-                               cpuport=CPU_PORT, pipeconf=pipeconf_name) #elogger=True)
+                               cpuport=CPU_PORT, pipeconf=pipeconf_name, loglevel="debug")
         s3 = self.addSwitch('s3', cls=ONOSBmv2Switch, grpcport=50003, thriftport=50006,
-                               cpuport=CPU_PORT, pipeconf=pipeconf_name) #elogger=True)
+                               cpuport=CPU_PORT, pipeconf=pipeconf_name, loglevel="debug")
 
         # Switch Links
         self.addLink(s1, s2)
         self.addLink(s1, s3)
 
 
-        # IPv6 hosts attached to leaf 1
+        # IPv4 switches
         h1 = self.addHost('h1', cls=None, mac="00:00:00:00:00:01",
                            ipv4='10.0.0.1/32')
         h2 = self.addHost('h2', cls=None, mac="00:00:00:00:00:02",
@@ -64,12 +54,12 @@ class TutorialTopo(Topo):
 
 def main(argz):
     topo = TutorialTopo()
-    controller = RemoteController('c0', ip=argz.onos_ip)
-    #controller = RemoteController('c0', ip='127.0.0.1')
+    #controller = RemoteController('c0', ip=argz.onos_ip)
+    controller = RemoteController('c0', ip='127.0.0.1')
 
-    net = Mininet(topo=topo, controller=None)
-    #net = Mininet(topo=topo, controller=lambda name: controller, listenPort=6633)
-    net.addController(controller)
+    #net = Mininet(topo=topo, controller=None)
+    net = Mininet(topo=topo, controller=lambda name: controller, listenPort=6633)
+    #net.addController(controller)
 
     net.start()
     CLI(net)
